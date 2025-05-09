5004ed1a68c7bdc3ad9a500cdfd50fa9e39b4a33
ji-hus <108216456+ji-hus@users.noreply.github.com>
2025-05-09 00:20:16 -0400

    Update menu items

--- src/data/menu-items.ts
+++ src/data/menu-items.ts
@@ -481,5 +481,45 @@ export const menuItems: MenuItem[] = [
     "createdAt": "2025-05-09T02:24:20.032Z",
     "updatedAt": "2025-05-09T04:05:30.321Z",
     "version": 4
+  },
+  {
+    "id": "5532422b-41e6-4338-8bbd-364913c09d0d",
+    "name": "Classic Sourdough",
+    "description": "Crusty outside, tangy inside. Fermented for 48 hours with our well established starter.",
+    "price": "9",
+    "category": "Breads",
+    "stock": 0,
+    "madeToOrder": false,
+    "available": true,
+    "active": true,
+    "dietaryInfo": {
+      "vegan": false,
+      "glutenFree": false,
+      "nutFree": false,
+      "dairyFree": false,
+      "halal": false,
+      "kosher": false
+    },
+    "allergens": {
+      "wheat": false,
+      "nuts": false,
+      "coconut": false,
+      "milk": false,
+      "eggs": false,
+      "soy": false,
+      "sesame": false,
+      "shellfish": false,
+      "fish": false,
+      "peanuts": false,
+      "treeNuts": false,
+      "sulfites": false
+    },
+    "isSpecial": false,
+    "bestSeller": false,
+    "seasonal": false,
+    "image": "",
+    "createdAt": "2025-05-09T04:20:15.747Z",
+    "updatedAt": "2025-05-09T04:20:15.747Z",
+    "version": 1
   }
 ];
